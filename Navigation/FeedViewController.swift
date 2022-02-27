0004f27d4fabb7012a238a33ad7a821ab80d9161
karginator <maksim.kargin@gmail.com>
2022-02-27 18:01:27 +0700

    выполнил дополнительное задание, коммит на сдачу ДЗ 2.2

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -17,14 +17,50 @@ class FeedViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let stackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            stackView.axis = .vertical
+            stackView.spacing = 10
+            stackView.distribution = .fillEqually
+            stackView.alignment = .fill
+            stackView.backgroundColor = .systemGray2
+            return stackView
+        } ()
+        view.addSubview(stackView)
         
-        let postButton = UIButton(frame: CGRect(x: 0, y: 100, width: 100, height: 50))
-        postButton.setTitle("Post", for: .normal)
-        postButton.backgroundColor = .white
-        postButton.layer.cornerRadius = 4
-        postButton.setTitleColor(.black, for: .normal)
-        postButton.addTarget(self, action: #selector(tap), for: .touchUpInside)
-        view.addSubview(postButton)
+        [
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -16),
+            stackView.heightAnchor.constraint(equalToConstant: 100)
+        ]
+            .forEach {$0.isActive = true}
+        
+        let postButtonOne: UIButton = {
+            let button = UIButton(frame: CGRect(x: 0, y: 100, width: 100, height: 50))
+            button.setTitle("Post One", for: .normal)
+            button.backgroundColor = .white
+            button.layer.cornerRadius = 4
+            button.setTitleColor(.black, for: .normal)
+            button.addTarget(self, action: #selector(tap), for: .touchUpInside)
+            return button
+        } ()
+        
+        stackView.addArrangedSubview(postButtonOne)
+        
+        let postButtonTwo: UIButton = {
+            let button = UIButton(frame: CGRect(x: 0, y: 100, width: 100, height: 50))
+            button.setTitle("Post Two", for: .normal)
+            button.backgroundColor = .white
+            button.layer.cornerRadius = 4
+            button.setTitleColor(.black, for: .normal)
+            button.addTarget(self, action: #selector(tap), for: .touchUpInside)
+            return button
+        } ()
+        
+        stackView.addArrangedSubview(postButtonTwo)
     }
     
     @objc func tap() {
